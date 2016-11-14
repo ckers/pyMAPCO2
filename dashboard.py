89970c3df6f4db2698d4bb3d01abd2179a0217cb
Colin Dietrich <repos@wildjuniper.com>
2016-11-14 10:53:22 -0800

    Basic port scan for mapco2

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,17 @@
 
 
-from bokeh.plotting import figure
+#from bokeh.plotting import figure
 
-p = figure(plot_height=600, plot_width=700, title="", toolbar_location=None, tools=[hover])
+#p = figure(plot_height=600, plot_width=700, title="", toolbar_location=None, tools=[hover])
+
+from gserial import scan
+
+ss = scan.Scan()
+ss.scan()
+print('All ports found:')
+print(ss.current_ports)
+
+i = scan.ID(hint=['0005', 'pCO2'], poll=b'\r')
+i.baudrates = [9600]
+p, b = i.get(verbose=True)
+print('Port found with config:', p, b)
